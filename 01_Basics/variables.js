00882871f29f78552f88480a72810ecc8147ca2d
Vivek_Prajapati <vivek22prajapati@gmail.com>
2026-06-30 06:34:01 +0000

    Add varible and datatypes

---
+++ 01_Basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 647623;
+let accountEmail = "vivek@gmail"
+var accountpass = "123"
+accountcity = "surat"
+let accountsate;
+
+console.log(accountId);
+
+//accountId = 43;
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountcity,accountsate])
+
+accountEmail = "kf@gmail.com"
+accountpass = "434"
+accountcity = "jaipur"
+accountsate = "gujart"
+
+console.table([accountId,accountEmail,accountpass,accountsate,accountcity])
