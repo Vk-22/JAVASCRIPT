f23c4ec8dffff9669f19a627f79b060f7119975d
Vivek_Prajapati <vivek22prajapati@gmail.com>
2026-07-01 05:35:34 +0000

    Operations

---
+++ 01_Basics/Operations.js
@@ -0,0 +1,14 @@
+let str1 = "vive"
+let str2 = "k"
+let str3 = (str1 + str2)
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+let counter = 100
+counter++;
+console.log(counter)
