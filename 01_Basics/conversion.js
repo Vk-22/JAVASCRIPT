a9eba420fceff3881cee573750ac203fea768337
Vivek_Prajapati <vivek22prajapati@gmail.com>
2026-07-01 05:10:56 +0000

    Conversion

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,27 @@
+let score = "33"
+console.log(typeof score)
+
+let valueNumber = Number(score)
+console.log(valueNumber)
+console.log(typeof valueNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = 1
+let Login = "vivek"
+let Logged = Boolean(isLoggedIn)
+let log = Boolean(Login)
+console.log(Logged)
+console.log(log)
+console.log(typeof log)
+
+// 1 => True 0 => false
+// "" => false
+// "vivek" => true
+
+let number = 33
+let Num = String(number)
+console.log(typeof Num)
+console.log(Num)
