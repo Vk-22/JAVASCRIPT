abe7deb7cde8e1efc85d23c1e56151dd35461db5
Vivek_Prajapati <vivek22prajapati@gmail.com>
2026-07-03 05:22:55 +0000

    summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,26 @@
+// Primitive 
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol("123")
+const id2 = Symbol("123")
+
+console.log(id === id2)
+
+
+// Non Primitive
+
+// Array, Objects, Functions
+
+const hero = ["vivek", "raj", "rajul"]
+
+let object = {
+    name: "vivek",
+    age: 21,
+    kg: 50
+}
+
+const myfunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myfunction)
