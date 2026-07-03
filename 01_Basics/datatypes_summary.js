60b69d030b2e8021d0a2e27a53be0cb2f5fd1646
Vivek_Prajapati <vivek22prajapati@gmail.com>
2026-07-03 05:58:09 +0000

    stack

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -24,3 +24,4 @@ const myfunction = function(){
 }
 
 console.log(typeof myfunction)
+
---
+++ 01_Basics/stack_heap.js
@@ -0,0 +1,24 @@
+let myname = "vivek"
+let yourname = myname
+
+myname = "kadvi"
+
+console.log(myname)
+console.log(yourname)
+
+
+// For heap memmory
+
+let myobject = {
+    email: "vk22gmail.com",
+    id: 777
+}
+//console.log(myobject)
+
+let yourobject = myobject
+
+yourobject.email = "harsj62gmail.com"
+
+console.log(myobject.email)
+console.log(yourobject.email)
+
