843d4ea9fdc6d01386e82c9e0578e74850aaca52
Vivek_Prajapati <vivek22prajapati@gmail.com>
2026-07-18 14:39:56 +0000

    loops

---
+++ 05_Iteratio/loops.js
@@ -0,0 +1,9 @@
+// loops
+
+for(let index = 1; index<=20; index++){
+    if(index === 5){
+        console.log("BASANTI AB RUK JAO")
+        break;
+    }
+    console.log(`Value of index is ${index}`)
+}
